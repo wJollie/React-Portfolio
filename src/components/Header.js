b2b884d020894ba99a2fc3d87b5c2c97abc7d926
wjollie <william.jollie23@gmail.com>
2023-10-12 19:01:11 -0400

    -added Header.js and Navigation.js code

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+// import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <h1>William Jollie</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/about-me">About Me</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Me</Link>
+          </li>
+          <li>
+            <Link to="/resume">Resume</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Navigation.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+import { Link, useLocation } from "react-router-dom";
+
+function Navigation() {
+  const location = useLocation();
+
+  return (
+    <nav>
+      <ul>
+        <li className={location.pathname === "/about-me" ? "active" : ""}>
+          <Link to="/about-me">About Me</Link>
+        </li>
+        <li className={location.pathname === "/portfolio" ? "active" : ""}>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li className={location.pathname === "/contact" ? "active" : ""}>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li className={location.pathname === "/resume" ? "active" : ""}>
+          <Link to="/resume">Resume</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
