9011b81a89d12da82a7e4205f9601e0dac695dbb
wjollie <william.jollie23@gmail.com>
2023-10-17 21:39:59 -0400

    -POST

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -66,7 +66,7 @@ const Contact = () => {
       {isSubmitted ? (
         <p className='thankYou'>Thank you! Your message has been saved.</p>
       ) : (
-        <form onSubmit={handleSubmit} name="contact" data-netlify="true">
+        <form onSubmit={handleSubmit} name="contact" method="POST" data-netlify="true">
           <div>
             <label htmlFor="name">Name:</label>
             <input
