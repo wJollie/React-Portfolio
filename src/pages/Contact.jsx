d71a27fab7eae3e7e18da14c9fce19cd2604ad8d
wjollie <william.jollie23@gmail.com>
2023-10-17 23:13:13 -0400

    -test

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -66,7 +66,7 @@ const Contact = () => {
       {isSubmitted ? (
         <p className='thankYou'>Thank you! Your message has been saved.</p>
       ) : (
-        <form name="contact" method="post" action="/">
+        <form name="contact" method="post" onSubmit={handleSubmit}>
           <input type="hidden" name="form-name" value="contact" />
           <div>
             <label htmlFor="name">Name:</label>
