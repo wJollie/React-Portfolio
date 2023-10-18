4363bb7f39e486cc84a650caab0f3ed0dec27226
wjollie <william.jollie23@gmail.com>
2023-10-17 21:49:39 -0400

    -form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,65 +6,29 @@ const Contact = () => {
     email: '',
     message: '',
   });
-  const [errors, setErrors] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
-    if (errors[name]) {
-      setErrors({ ...errors, [name]: '' });
-    }
-  };
-
-  const validateEmail = (email) => {
-    // Simple email format validation
-    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-    return regex.test(email);
-  };
-
-  const handleBlur = (e) => {
-    const { name, value } = e.target;
-    if (!value) {
-      setErrors({ ...errors, [name]: 'This field is required' });
-    } else if (name === 'email' && !validateEmail(value)) {
-      setErrors({ ...errors, [name]: 'Invalid email format' });
-    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const newErrors = {};
 
-    // Validate required fields
-    if (!formData.name) {
-      newErrors.name = 'Name is required';
-    }
-    if (!formData.email) {
-      newErrors.email = 'Email is required';
-    } else if (!validateEmail(formData.email)) {
-      newErrors.email = 'Invalid email format';
-    }
-    if (!formData.message) {
-      newErrors.message = 'Message is required';
-    }
+    // The form is handled by Netlify, so you don't need to specify the form action.
+    // Just make sure to use the 'name' attribute to identify the form.
 
-    if (Object.keys(newErrors).length === 0) {
-      // You don't need to specify the form action here because Netlify will handle it for you.
-      // Just set the `data-netlify="true"` attribute on the form.
-      // Clear the form and set the submitted state
-      setFormData({ name: '', email: '', message: '' });
-      setIsSubmitted(true);
-    } else {
-      setErrors(newErrors);
-    }
+    // Clear the form and set the submitted state
+    setFormData({ name: '', email: '', message: '' });
+    setIsSubmitted(true);
   };
 
   return (
     <div>
-      <h1 className='contactMeText'>Contact Me!</h1>
+      <h1 className="contactMeText">Contact Me!</h1>
       {isSubmitted ? (
-        <p className='thankYou'>Thank you! Your message has been saved.</p>
+        <p className="thankYou">Thank you! Your message has been saved.</p>
       ) : (
         <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
           <div>
@@ -75,21 +39,19 @@ const Contact = () => {
               name="name"
               value={formData.name}
               onChange={handleInputChange}
-              onBlur={handleBlur}
+              required
             />
-            {errors.name && <p className="error">{errors.name}</p>}
           </div>
           <div>
             <label htmlFor="email">Email:</label>
             <input
-              type="text"
+              type="email"
               id="email"
               name="email"
               value={formData.email}
               onChange={handleInputChange}
-              onBlur={handleBlur}
+              required
             />
-            {errors.email && <p className="error">{errors.email}</p>}
           </div>
           <div>
             <label htmlFor="message">Message:</label>
@@ -98,9 +60,8 @@ const Contact = () => {
               name="message"
               value={formData.message}
               onChange={handleInputChange}
-              onBlur={handleBlur}
+              required
             />
-            {errors.message && <p className="error">{errors.message}</p>}
           </div>
           <button type="submit">Submit</button>
         </form>
