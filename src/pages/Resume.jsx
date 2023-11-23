f4cff35dee8f6f20c6a725b04535647e66c3bc39
wjollie <william.jollie23@gmail.com>
2023-11-23 10:37:55 -0500

    -changed resume (might not work)

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -6,7 +6,7 @@ const Resume = () => {
       <h1>Resume</h1>
       <p className="resumeError">Resume download not available on mobile</p>
       <embed className="resume"
-        src="https://drive.google.com/uc?id=1s3wip6OGJz10_k9h90_CUSFSEMUA-nPM"
+        src="./projectImages/William Jollie's Resume LATEST.pdf"
         type="application/pdf"
         width="90%"
         height="800px"
